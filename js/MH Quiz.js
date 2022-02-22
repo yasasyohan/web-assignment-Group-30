9f3dc3e18ac0233e0ff483c85a1037076b3e378b
warunadahampriya <warun00daham@gmail.com>
2022-02-23 01:19:54 +0530

    Setup Answer Box

--- js/MH Quiz.js
+++ js/MH Quiz.js
@@ -58,3 +58,66 @@ restart_quiz.onclick = ()=>{
     startTimerLine(widthValue);
     next_btn.style.display = "none";
 }
+
+quit_quiz.onclick = ()=>{
+    window.location.reload();
+}
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+    que_count++;
+    que_numb++;
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    next_btn.style.display = "none";
+    }else{
+        console.log("Questions completed");
+        showResultBox();
+    }
+}
+ 
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb + "." + questions[index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].Options[0] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].Options[1] +'<span></span></div>'
+                     /*+ '<div class="option"><span></span></div>'
+                     + '<div class="option"><span></span></div>'*/
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if(userAns == correctAns){
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct");
+        console.log("Answer is Correct");
+    }else{
+        answer.classList.add("incorrect");
+        console.log("Answer is Wrong");
+    }
+    for (let i = 0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.style.display = "block";
+}
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
