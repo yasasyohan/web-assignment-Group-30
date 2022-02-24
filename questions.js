07bb8aee635e410597b6d2ba2cc41d1b2fd75f4a
warunadahampriya <warun00daham@gmail.com>
2022-02-24 21:40:17 +0530

    Final files

---
+++ questions.js
@@ -0,0 +1,137 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Do you feel overwhelmingly sad at times",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 2,
+        question: "When you think of the future you feel hopeless",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Do you feel like a complete failure?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Do you get a lot of satisfaction / joy from doing things?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 5,
+        question: "Do you feel guilty about something most of the time?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Do you feel like  you being punished?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 7,
+        question: "Do you feel disappointed (even disgusted) with yourself?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 8,
+        question: "The bad things in your life aren’t all your fault",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 9,
+        question: "Do you oftening on the brink of tears or cry?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Do you feel irritated and annoyed by things in your life?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 11,
+        question: "you very interested in other people’s lives and do you like to listen to them",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 12,
+        question: "Do you find it easy to make decisions, big and small?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Do you think you unattractive or ugly?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    }, 
+    {
+        numb: 14,
+        question: "Do you find it really hard to do anything, especially work?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Do you sleep patterns have been really disrupted?",
+        answer: "Yes",
+        Options: [
+            "Yes",
+            "No"
+        ]
+    },
+];
